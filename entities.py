26c2ed5ba69d7801642a0754bbbb72acbe5cb5d3
Carl Sterner <carl.sterner@skola.taby.se>
2024-11-21 13:48:07 +0100

    buggfix pt2

--- entities.py
+++ entities.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class föremål:
     def __init__(self):
         self.items = []
@@ -38,46 +37,4 @@ class player:
 class monster:
     def __init__(self, str, cords):
         self.str = str
-        self.cords = cords
-=======
-class föremål:
-    def __init__(self):
-        self.items = []
-        self.items_pos = []
-
-    def Placera(self, typ, pos):
-        self.items.append(typ)
-        self.items_pos.append(pos)
-
-class player:
-    def __init__(self, hp, lvl, str, skill, pos, inventory = ["ab", "bc", "cfd", "dfc", "fab", "bfc", "cdv", "dvc", "avb"]):
-        self.hp = hp
-        self.lvl = lvl
-        self.str = str
-        self.skill = skill
-        self.inventory = inventory
-        self.pos = pos
-   
-    def Move(self, direction):
-        try:
-            if direction == "Norr":
-                self.pos -= 1
-            elif direction == "Syd":
-                self.pos += 1
-            elif direction == "Öst":
-                self.pos -= 10
-            elif direction == "Väst":
-                self.pos += 10
-        except:
-            print("fel i player.Move()")
-    
-    def levelup(self, amount):
-        self.lvl += amount
-
-   
-
-class monster:
-    def __init__(self, str, cords):
-        self.str = str
->>>>>>> a1751aa65a32e93f93fff66bf3845984f6cff1a9
         self.cords = cords
